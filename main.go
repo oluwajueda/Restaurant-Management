ed9446d5031748f30e6f06677d00c9b920822c79
oluwajueda <oluwajuedaloade@gmail.com>
2023-04-16 13:39:47 +0100

    create database for file

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import(
 "os"
+"github.com/gin-gonic/gin"
 "golang-restaurant-management/database"
+"golang-restaurant-management/routes"
+"golang-restaurant-management/middleware"
+"golang-restaurant-management/mongo"
 
 )
 
-
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+ 
 func main(){
 port := os.Getenv("PORT")
 
@@ -15,6 +20,16 @@ if port == ""{
 }
 
 router := gin.New()
-router.use(gin.Logger())
+router.Use(gin.Logger())
+routes.UserRoutes(router)
+router.Use(middleware.Authentication())
 
+routes.FoodRoutes(router)
+routes.MenuRoutes(router)
+routes.TableRoutes(router)
+routes.OrderRoutes(router)
+routes.OrderItemRoutes(router)
+routes.InvoiceRoutes(router)
+
+router.Run(":" + port)
 }
--- routes/invoiceRouter.go
+++ routes/invoiceRouter.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	controller "golang-restaurant-management/controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
